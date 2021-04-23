74dc0b62beac9c9e609b8175a4b45d8443940db2
Lewis Dale <lewis.dale@madetech.com>
2021-04-23 15:03:14 +0100

    Test and implementation for mapping Group into GiasDataResponse

--- TramsDataApi.Test/Factories/TrustResponseFactoryTests.cs
+++ TramsDataApi.Test/Factories/TrustResponseFactoryTests.cs
@@ -12,16 +12,59 @@ namespace TramsDataApi.Test.Factories
         [Fact]
         public void TrustResponseFactory_CreatesTrustResponse_FromAGroup()
         {
-            var group = new Group();
+            var group = GenerateGroup();
             
             var ifdDataResponse = new IFDDataResponse();
-            var giasDataResponse = new GIASDataResponse();
+            var giasDataResponse = new GIASDataResponse
+            {
+                GroupId = group.GroupId,
+                GroupName = group.GroupName,
+                CompaniesHouseNumber = group.CompaniesHouseNumber,
+                GroupContactAddress = new AddressResponse
+                {
+                    Street = group.GroupContactStreet,
+                    AdditionalLine = group.GroupContactAddress3,
+                    Locality = group.GroupContactLocality,
+                    Town = group.GroupContactTown,
+                    County = group.GroupContactCounty,
+                    Postcode = group.GroupContactPostcode
+                },
+                Ukprn = group.Ukprn
+            };
             var academyResponses = new List<AcademyResponse>();
             var expected = new TrustResponse
                 {IfdData = ifdDataResponse, GiasData = giasDataResponse, Academies = academyResponses};
 
             var result = TrustResponseFactory.Create(group);
             expected.Should().BeEquivalentTo(result);
+        }
+    
+        private Group GenerateGroup()
+        {
+            return new Group
+            {
+                GroupUid = "1",
+                GroupId = "1",
+                GroupName = "Test Group",
+                CompaniesHouseNumber = "011013254",
+                GroupTypeCode = "5",
+                GroupType = "FS",
+                ClosedDate = "01/01/1970",
+                GroupStatusCode = "45",
+                GroupStatus = "CS",
+                GroupContactStreet = "Street Name",
+                GroupContactLocality = "Locality",
+                GroupContactAddress3 = "Address 3",
+                GroupContactTown = "Town Name",
+                GroupContactCounty = "County Name",
+                GroupContactPostcode = "P05 7CD",
+                HeadOfGroupTitle = "Mx",
+                HeadOfGroupFirstName = "First Name",
+                HeadOfGroupLastName = "Last Name",
+                Ukprn = "ukprn",
+                IncorporatedOnOpenDate = "01/01/1970",
+                OpenDate = "01/01/1970"
+            };
         }
     }
 }
--- TramsDataApi/Factories/TrustResponseFactory.cs
+++ TramsDataApi/Factories/TrustResponseFactory.cs
@@ -9,7 +9,22 @@ namespace TramsDataApi.Factories
         public static TrustResponse Create(Group group)
         {
             var ifdDataResponse = new IFDDataResponse();
-            var giasDataResponse = new GIASDataResponse();
+            var giasDataResponse = new GIASDataResponse
+            {
+                GroupId = group.GroupId,
+                GroupName = group.GroupName,
+                CompaniesHouseNumber = group.CompaniesHouseNumber,
+                GroupContactAddress = new AddressResponse
+                {
+                    Street = group.GroupContactStreet,
+                    AdditionalLine = group.GroupContactAddress3,
+                    Locality = group.GroupContactLocality,
+                    Town = group.GroupContactTown,
+                    County = group.GroupContactCounty,
+                    Postcode = group.GroupContactPostcode
+                },
+                Ukprn = group.Ukprn
+            };
             var academyResponses = new List<AcademyResponse>();
             return new TrustResponse
                 {IfdData = ifdDataResponse, GiasData = giasDataResponse, Academies = academyResponses};
