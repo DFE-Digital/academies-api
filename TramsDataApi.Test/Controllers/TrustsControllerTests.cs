8bfba89495e82e8afc80e3a5e9add43922a5769c
Lewis Dale <lewis.dale@madetech.com>
2021-04-27 14:13:39 +0100

    Added unit test for retrieving existing trust by ukprn

--- TramsDataApi.Test/Controllers/TrustsControllerTests.cs
+++ TramsDataApi.Test/Controllers/TrustsControllerTests.cs
@@ -11,7 +11,7 @@ namespace TramsDataApi.Test.Controllers
     public class TrustsControllerTests
     {
         [Fact]
-        public void GetTrustsByUkPrn_ReturnsNull_WhenNoTrustsFound()
+        public void GetTrustsByUkPrn_ReturnsNotFoundResult_WhenNoTrustsFound()
         {
             var gateway = new Mock<ITrustGateway>();
             var ukprn = "mockukprn";
@@ -21,6 +21,40 @@ namespace TramsDataApi.Test.Controllers
             var result = controller.Get(ukprn);
 
             result.Should().BeOfType(typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public void GetTrustsByUkprn_ReturnsTrustResponse_WhenTrustFound()
+        {
+            var ukprn = "mockukprn";
+
+            var trustResponse = new TrustResponse
+            {
+                IfdData = new IFDDataResponse(),
+                GiasData = new GIASDataResponse
+                {
+                    GroupId = "Test group ID",
+                    GroupName = "Test group name",
+                    CompaniesHouseNumber = "Test CH Number",
+                    GroupContactAddress = new AddressResponse
+                    {
+                        Street = "Test street",
+                        AdditionalLine = "Test additional line",
+                        Locality = "Test locality",
+                        Town = "Test town",
+                        County = "Test county",
+                        Postcode = "P05TC0D"
+                    },
+                    Ukprn = ukprn
+                }
+            };
+            var gateway = new Mock<ITrustGateway>();
+            gateway.Setup(g => g.GetByUkprn(ukprn)).Returns(trustResponse);
+            
+            var controller = new TrustsController(gateway.Object);
+            var result = controller.Get(ukprn);
+
+            result.Should().BeEquivalentTo(new OkObjectResult(trustResponse));
         }
     }
 }
