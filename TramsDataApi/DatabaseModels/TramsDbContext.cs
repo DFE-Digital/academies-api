66b883d405c092c8ed55141f0c0ad6f66f00dfa4
Lewis Dale <lewis.dale@madetech.com>
2021-05-13 16:13:39 +0100

    Removed autogenerated content from database models

--- TramsDataApi/DatabaseModels/TramsDbContext.cs
+++ TramsDataApi/DatabaseModels/TramsDbContext.cs
@@ -2,10 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
-
 namespace TramsDataApi.DatabaseModels
 {
     public partial class TramsDbContext : DbContext
