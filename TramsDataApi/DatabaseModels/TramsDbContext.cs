262dd03053ea7336f8c4da6b04f0d0b81cc5e0f6
Lewis <lewis.dale@madetech.com>
2021-05-20 10:57:59 +0100

    Added TramsDbContext containing only tables that require creating

---
+++ TramsDataApi/DatabaseModels/TramsDbContext.cs
@@ -0,0 +1,99 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TramsDataApi.DatabaseModels
+{
+    public partial class TramsDbContext : DbContext
+    {
+        public TramsDbContext()
+        {
+        }
+
+        public TramsDbContext(DbContextOptions<TramsDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public virtual DbSet<AcademyTransferProjectIntendedTransferBenefits> AcademyTransferProjectIntendedTransferBenefits { get; set; }
+        public virtual DbSet<AcademyTransferProjects> AcademyTransferProjects { get; set; }
+        public virtual DbSet<TransferringAcademies> TransferringAcademies { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=local_trams_test_db;persist security info=True;User id=sa; Password=StrongPassword905");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AcademyTransferProjectIntendedTransferBenefits>(entity =>
+            {
+                entity.ToTable("AcademyTransferProjectIntendedTransferBenefits", "sdd");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.FkAcademyTransferProjectId).HasColumnName("fk_AcademyTransferProjectId");
+
+                entity.Property(e => e.SelectedBenefit).IsRequired();
+
+                entity.HasOne(d => d.FkAcademyTransferProject)
+                    .WithMany(p => p.AcademyTransferProjectIntendedTransferBenefits)
+                    .HasForeignKey(d => d.FkAcademyTransferProjectId)
+                    .HasConstraintName("FK__AcademyTr__fk_Ac__4316F928");
+            });
+
+            modelBuilder.Entity<AcademyTransferProjects>(entity =>
+            {
+                entity.HasKey(e => e.Urn)
+                    .HasName("PK__AcademyT__C5B214360AF6201A");
+
+                entity.ToTable("AcademyTransferProjects", "sdd");
+
+                entity.HasIndex(e => e.ProjectNumber)
+                    .HasName("AcademyTransferProjectNumber");
+
+                entity.HasIndex(e => e.Urn)
+                    .HasName("AcademyTransferProjectUrn");
+
+                entity.Property(e => e.HtbDate).HasColumnType("date");
+
+                entity.Property(e => e.OutgoingTrustUkprn)
+                    .IsRequired()
+                    .HasMaxLength(8);
+
+                entity.Property(e => e.ProjectNumber)
+                    .HasMaxLength(7)
+                    .IsUnicode(false)
+                    .HasComputedColumnSql("('AT-'+right('0000'+CONVERT([varchar](20),[Urn]),(4)))");
+
+                entity.Property(e => e.TargetDateForTransfer).HasColumnType("date");
+
+                entity.Property(e => e.TransferFirstDiscussed).HasColumnType("date");
+            });
+
+            modelBuilder.Entity<TransferringAcademies>(entity =>
+            {
+                entity.ToTable("TransferringAcademies", "sdd");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.FkAcademyTransferProjectId).HasColumnName("fk_AcademyTransferProjectId");
+
+                entity.Property(e => e.IncomingTrustUkprn).HasMaxLength(8);
+
+                entity.Property(e => e.OutgoingAcademyUkprn)
+                    .IsRequired()
+                    .HasMaxLength(8);
+
+                entity.HasOne(d => d.FkAcademyTransferProject)
+                    .WithMany(p => p.TransferringAcademies)
+                    .HasForeignKey(d => d.FkAcademyTransferProjectId)
+                    .HasConstraintName("FK__Transferr__fk_Ac__403A8C7D");
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
