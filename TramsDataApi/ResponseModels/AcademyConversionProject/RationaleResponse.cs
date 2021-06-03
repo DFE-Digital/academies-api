41a9ae5a7431e319f29dab3d09a7617f1a4edf18
rebecca fitzsimmons <rebecca@madetech.com>
2021-06-03 17:17:36 +0100

    Add rationale fields to academy conversion endpoint response

---
+++ TramsDataApi/ResponseModels/AcademyConversionProject/RationaleResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TramsDataApi.ResponseModels.AcademyConversionProject
+{
+	public class RationaleResponse
+	{
+		public string ProjectRationale { get; set; }
+		public string TrustRationale { get; set; }
+	}
+}
