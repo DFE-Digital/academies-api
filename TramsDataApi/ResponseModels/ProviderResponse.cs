af945fd6438bd3c122c12c068c4d43959f23bbb1
nouriach <49981579+nouriach@users.noreply.github.com>
2021-07-16 14:34:13 +0100

    Create new response model for Provider

--- TramsDataApi/ResponseModels/ProviderResponse.cs
+++ TramsDataApi/ResponseModels/ProviderResponse.cs
@@ -1,5 +1,11 @@
 namespace TramsDataApi.ResponseModels
 {
     public class ProviderResponse
-    {}
+    {
+        public int Urn { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Group { get; set; }
+        public int? Ukprn { get; set; }
+    }
 }
