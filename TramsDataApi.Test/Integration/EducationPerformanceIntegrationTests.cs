2c6f68ecf1675e728fc335fbf8d6d7433759850b
Robert McHugh <robert@madetech.com>
2021-08-12 09:33:39 +0100

    fix test expected

--- TramsDataApi.Test/Integration/EducationPerformanceIntegrationTests.cs
+++ TramsDataApi.Test/Integration/EducationPerformanceIntegrationTests.cs
@@ -719,7 +719,7 @@ namespace TramsDataApi.Test.Integration
                 },
                 LAAverageP8LowerConfidence = laEducationPerformance2.SipProgress8lowerconfidence,
                 LAAverageP8UpperConfidence = laEducationPerformance2.SipProgress8upperconfidence,
-                LAEnteringEbacc = laEducationPerformance2.SipEnteringEbaccEngland
+                LAEnteringEbacc = laEducationPerformance1.SipEnteringEbaccEngland
             };
 
             var expectedKeyStage5Response = new KeyStage5PerformanceResponse
