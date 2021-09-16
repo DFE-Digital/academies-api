5db807649fa824cca10220902b9f70e229dc26d4
Robert McHugh <robert@madetech.com>
2021-09-16 10:31:02 +0100

    Reset IAcpGateway

--- TramsDataApi/Gateways/IAcademyConversionProjectGateway.cs
+++ TramsDataApi/Gateways/IAcademyConversionProjectGateway.cs
@@ -6,8 +6,8 @@ namespace TramsDataApi.Gateways
     public interface IAcademyConversionProjectGateway
     {
         AcademyConversionProject GetById(int id);
-        IEnumerable<AcademyConversionJoinModel> GetProjects(int take);
+        IEnumerable<AcademyConversionProject> GetProjects(int take);
         AcademyConversionProject Update(AcademyConversionProject academyConversionProject);
-        IEnumerable<AcademyConversionJoinModel> GetByStatuses(int take, List<string> state);
+        IEnumerable<AcademyConversionProject> GetByStatuses(int take, List<string> state);
     }
 }
