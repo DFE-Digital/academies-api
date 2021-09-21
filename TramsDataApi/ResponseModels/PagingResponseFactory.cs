0bcb373377e6c854c7938048c19a52e6e7d3d7dd
Martin Wheeler <martin.wheeler@education.gov.uk>
2021-09-21 10:50:37 +0100

    Update paging response factory to use the VirtualPath as path base is now default populated with string.empty

--- TramsDataApi/ResponseModels/PagingResponseFactory.cs
+++ TramsDataApi/ResponseModels/PagingResponseFactory.cs
@@ -28,15 +28,7 @@ namespace TramsDataApi.ResponseModels
                 {nameof(count), $"{count}"}
             };
 
-            var uriBuilder = new UriBuilder
-            {
-                Scheme = request.Scheme,
-                Path = request.Path,
-                Query = queryBuilder.ToString()
-            };
-            if (request.Host.Port != null) uriBuilder.Port = request.Host.Port.Value;
-
-            pagingResponse.NextPageUrl = uriBuilder.ToString();
+            pagingResponse.NextPageUrl = $"{request.GetDisplayUrl().Split('?')[0]}{queryBuilder}";
 
             return pagingResponse;
         }
