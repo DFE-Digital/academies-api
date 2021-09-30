aa47e32be8a6c244a3cf7f2f7ba3fbafb7747c55
Daniel Burnley <dan@danops.uk>
2021-09-30 12:15:47 +0100

    Add migration to set academy type and route where it's not already set

---
+++ TramsDataApi/Migrations/TramsDb/20210930092700_A2B-SetExistingConversionTypes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class A2BSetExistingConversionTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "sdd",
+                table: "AcademyConversionProject",
+                column: "AcademyTypeAndRoute",
+                value: "Converter",
+                keyColumn: "AcademyTypeAndRoute",
+                keyValue: null
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
