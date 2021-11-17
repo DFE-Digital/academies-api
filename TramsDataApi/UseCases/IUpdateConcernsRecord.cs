c989a3a81c0560c53e537d1d84f65326c8464554
Robert McHugh <robert@madetech.com>
2021-11-17 16:29:41 +0000

    add usecase to update concerns record

--- TramsDataApi/UseCases/IUpdateConcernsRecord.cs
+++ TramsDataApi/UseCases/IUpdateConcernsRecord.cs
@@ -1,7 +1,10 @@
+using TramsDataApi.RequestModels;
+using TramsDataApi.ResponseModels;
+
 namespace TramsDataApi.UseCases
 {
     public interface IUpdateConcernsRecord
     {
-        
+        ConcernsRecordResponse Execute(int urn, ConcernsRecordRequest request);
     }
 }
--- TramsDataApi/UseCases/UpdateConcernsRecord.cs
+++ TramsDataApi/UseCases/UpdateConcernsRecord.cs
@@ -1,4 +1,44 @@
-$HEADER$namespace $NAMESPACE$
+using TramsDataApi.Factories;
+using TramsDataApi.Gateways;
+using TramsDataApi.RequestModels;
+using TramsDataApi.ResponseModels;
+
+namespace TramsDataApi.UseCases
 {
-  public class $CLASS$ {$END$}
+    public class UpdateConcernsRecord : IUpdateConcernsRecord
+    {
+        private readonly IConcernsRecordGateway _concernsRecordGateway;
+        private readonly IConcernsCaseGateway _concernsCaseGateway;
+        private readonly IConcernsTypeGateway _concernsTypeGateway;
+        private readonly IConcernsRatingGateway _concernsRatingGateway;
+
+        public UpdateConcernsRecord(
+            IConcernsRecordGateway concernsRecordGateway,
+            IConcernsCaseGateway concernsCaseGateway,
+            IConcernsTypeGateway concernsTypeGateway,
+            IConcernsRatingGateway concernsRatingGateway)
+        {
+            _concernsRecordGateway = concernsRecordGateway;
+            _concernsCaseGateway = concernsCaseGateway;
+            _concernsTypeGateway = concernsTypeGateway;
+            _concernsRatingGateway = concernsRatingGateway;
+        }
+
+        public ConcernsRecordResponse Execute(int urn, ConcernsRecordRequest request)
+        {
+            var currentConcernsRecord = _concernsRecordGateway.GetConcernsRecordByUrn(urn);
+            var concernsCase = _concernsCaseGateway.GetConcernsCaseByUrn(request.CaseUrn) ??
+                               currentConcernsRecord.ConcernsCase;
+            var concernsType = _concernsTypeGateway.GetConcernsTypeByUrn(request.TypeUrn) ??
+                               currentConcernsRecord.ConcernsType;
+            var concernsRating = _concernsRatingGateway.GetRatingByUrn(request.RatingUrn) ??
+                                 currentConcernsRecord.ConcernsRating;
+            
+            var concernsCaseToUpdate = ConcernsRecordFactory
+                .Update(currentConcernsRecord, request, concernsCase, concernsType, concernsRating);
+            
+            var updatedConcernsRecord = _concernsRecordGateway.Update(concernsCaseToUpdate);
+            return ConcernsRecordResponseFactory.Create(updatedConcernsRecord);
+        }
+    }
 }
