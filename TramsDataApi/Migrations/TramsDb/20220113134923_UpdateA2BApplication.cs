f475b471a67a7833bcbf1291f7d6c0243028da27
Martin Wheeler <martin.wheeler@education.gov.uk>
2022-01-14 17:42:45 +0000

    removed unneeded migration update new migration to reflect changes in unneeded applied migration

--- TramsDataApi/Migrations/TramsDb/20220113134923_UpdateA2BApplication.cs
+++ TramsDataApi/Migrations/TramsDb/20220113134923_UpdateA2BApplication.cs
@@ -7,6 +7,54 @@ namespace TramsDataApi.Migrations.TramsDb
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "A2BApplication",
+                schema: "sdd");
+
+            migrationBuilder.CreateTable(
+                name: "A2BApplication",
+                schema: "sdd",
+                columns: table => new
+                {
+                    ApplicationId = table.Column<string>(nullable: false),
+                    ApplicationStatusId = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ApplicationType = table.Column<int>(nullable: true),
+                    FormTrustProposedNameOfTrust = table.Column<string>(nullable: true),
+                    ApplicationSubmitted = table.Column<bool>(nullable: true),
+                    ApplicationLeadAuthorId = table.Column<string>(nullable: true),
+                    ApplicationVersion = table.Column<string>(nullable: true),
+                    ApplicationLeadAuthorName = table.Column<string>(nullable: true),
+                    ApplicationLeadEmail = table.Column<string>(nullable: true),
+                    ApplicationRole = table.Column<string>(nullable: true),
+                    ApplicationRoleOtherDescription = table.Column<string>(nullable: true),
+                    ChangesToTrust = table.Column<bool>(nullable: true),
+                    ChangesToTrustExplained = table.Column<string>(nullable: true),
+                    ChangesToLaGovernance = table.Column<bool>(nullable: true),
+                    ChangesToLaGovernanceExplained = table.Column<string>(nullable: true),
+                    FormTrustOpeningDate = table.Column<DateTime>(nullable: true),
+                    TrustApproverName = table.Column<string>(nullable: true),
+                    TrustApproverEmail = table.Column<string>(nullable: true),
+                    TrustId = table.Column<string>(nullable: true),
+                    FormTrustReasonApprovalToConvertAsSat = table.Column<bool>(nullable: true),
+                    FormTrustReasonApprovedPerson = table.Column<string>(nullable: true),
+                    FormTrustReasonForming = table.Column<string>(nullable: true),
+                    FormTrustReasonVision = table.Column<string>(nullable: true),
+                    FormTrustReasonGeoAreas = table.Column<string>(nullable: true),
+                    FormTrustReasonFreedom = table.Column<string>(nullable: true),
+                    FormTrustReasonImproveTeaching = table.Column<string>(nullable: true),
+                    FormTrustPlanForGrowth = table.Column<string>(nullable: true),
+                    FormTrustPlansForNoGrowth = table.Column<string>(nullable: true),
+                    FormTrustGrowthPlansYesNo = table.Column<bool>(nullable: true),
+                    FormTrustImprovementSupport = table.Column<string>(nullable: true),
+                    FormTrustImprovementStrategy = table.Column<string>(nullable: true),
+                    FormTrustImprovementApprovedSponsor = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_A2BApplication", x => x.ApplicationId);
+                });
+            
             migrationBuilder.AlterColumn<bool>(
                 name: "KeyPersonFinancialDirector",
                 schema: "sdd",
@@ -48,55 +96,7 @@ namespace TramsDataApi.Migrations.TramsDb
                 schema: "sdd",
                 table: "A2BApplicationKeyPersons",
                 nullable: true);
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "FormTrustReasonApprovalToConvertAsSat",
-                schema: "sdd",
-                table: "A2BApplication",
-                nullable: true,
-                oldClrType: typeof(int),
-                oldType: "int",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "FormTrustGrowthPlansYesNo",
-                schema: "sdd",
-                table: "A2BApplication",
-                nullable: true,
-                oldClrType: typeof(int),
-                oldType: "int",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "ChangesToTrust",
-                schema: "sdd",
-                table: "A2BApplication",
-                nullable: true,
-                oldClrType: typeof(int),
-                oldType: "int",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "ChangesToLaGovernance",
-                schema: "sdd",
-                table: "A2BApplication",
-                nullable: true,
-                oldClrType: typeof(int),
-                oldType: "int",
-                oldNullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "ApplicationLeadEmail",
-                schema: "sdd",
-                table: "A2BApplication",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "ApplicationStatusId",
-                schema: "sdd",
-                table: "A2BApplication",
-                nullable: true);
-
+            
             migrationBuilder.CreateTable(
                 name: "A2BApplicationType",
                 schema: "sdd",
@@ -237,42 +237,52 @@ namespace TramsDataApi.Migrations.TramsDb
                 nullable: true,
                 oldClrType: typeof(bool),
                 oldNullable: true);
+            
+            migrationBuilder.DropTable(
+                name: "A2BApplication",
+                schema: "sdd");
 
-            migrationBuilder.AlterColumn<int>(
-                name: "FormTrustReasonApprovalToConvertAsSat",
+            migrationBuilder.CreateTable(
+                name: "A2BApplication",
                 schema: "sdd",
-                table: "A2BApplication",
-                type: "int",
-                nullable: true,
-                oldClrType: typeof(bool),
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<int>(
-                name: "FormTrustGrowthPlansYesNo",
-                schema: "sdd",
-                table: "A2BApplication",
-                type: "int",
-                nullable: true,
-                oldClrType: typeof(bool),
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<int>(
-                name: "ChangesToTrust",
-                schema: "sdd",
-                table: "A2BApplication",
-                type: "int",
-                nullable: true,
-                oldClrType: typeof(bool),
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<int>(
-                name: "ChangesToLaGovernance",
-                schema: "sdd",
-                table: "A2BApplication",
-                type: "int",
-                nullable: true,
-                oldClrType: typeof(bool),
-                oldNullable: true);
+                columns: table => new
+                {
+                    ApplicationId = table.Column<int>(nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ApplicationType = table.Column<string>(nullable: true),
+                    FormTrustProposedNameOfTrust = table.Column<string>(nullable: true),
+                    ApplicationSubmitted = table.Column<bool>(nullable: false),
+                    ApplicationLeadAuthorId = table.Column<string>(nullable: true),
+                    ApplicationVersion = table.Column<string>(nullable: true),
+                    ApplicationLeadAuthorName = table.Column<string>(nullable: true),
+                    ApplicationRole = table.Column<string>(nullable: true),
+                    ApplicationRoleOtherDescription = table.Column<string>(nullable: true),
+                    ChangesToTrust = table.Column<int>(nullable: true),
+                    ChangesToTrustExplained = table.Column<string>(nullable: true),
+                    ChangesToLaGovernance = table.Column<int>(nullable: true),
+                    ChangesToLaGovernanceExplained = table.Column<string>(nullable: true),
+                    FormTrustOpeningDate = table.Column<DateTime>(nullable: true),
+                    TrustApproverName = table.Column<string>(nullable: true),
+                    TrustApproverEmail = table.Column<string>(nullable: true),
+                    TrustId = table.Column<string>(nullable: true),
+                    FormTrustReasonApprovalToConvertAsSat = table.Column<int>(nullable: true),
+                    FormTrustReasonApprovedPerson = table.Column<string>(nullable: true),
+                    FormTrustReasonForming = table.Column<string>(nullable: true),
+                    FormTrustReasonVision = table.Column<string>(nullable: true),
+                    FormTrustReasonGeoAreas = table.Column<string>(nullable: true),
+                    FormTrustReasonFreedom = table.Column<string>(nullable: true),
+                    FormTrustReasonImproveTeaching = table.Column<string>(nullable: true),
+                    FormTrustPlanForGrowth = table.Column<string>(nullable: true),
+                    FormTrustPlansForNoGrowth = table.Column<string>(nullable: true),
+                    FormTrustGrowthPlansYesNo = table.Column<int>(nullable: true),
+                    FormTrustImprovementSupport = table.Column<string>(nullable: true),
+                    FormTrustImprovementStrategy = table.Column<string>(nullable: true),
+                    FormTrustImprovementApprovedSponsor = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_A2BApplication", x => x.ApplicationId);
+                });
         }
     }
 }
