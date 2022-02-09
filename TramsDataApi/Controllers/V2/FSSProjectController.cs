65701d7155d7d0b7e6e6eb44cd334e7dba74bb66
HussenShambeshDfE <97463113+HussenShambeshDfE@users.noreply.github.com>
2022-02-09 11:16:18 +0000

    removed commented unsued code

--- TramsDataApi/Controllers/V2/FSSProjectController.cs
+++ TramsDataApi/Controllers/V2/FSSProjectController.cs
@@ -21,23 +21,6 @@ namespace TramsDataApi.Controllers.V2
             _getAllFssProject = getAllFssProject;
         }
 
-        //[HttpGet("projects")]
-        //[MapToApiVersion("2.0")]
-        //public ActionResult<ApiResponseV2<FssProjectResponse>> GetAll(int page = 1, int count = 50)
-        //{
-        //    _logger.LogInformation($"Retreiving FSS Projects , page {page}, count {count}");
-
-        //    var projects = _getAllFssProject.Execute(page, count).ToList();
-
-        //    _logger.LogInformation( $"Found {count} projects, page {page}");
-
-        //    _logger.LogDebug(JsonSerializer.Serialize(projects));
-
-        //    var pagingResponse = PagingResponseFactory.Create(page, count, projects.Count, Request);
-        //    var response = new ApiResponseV2<FssProjectResponse>(projects.ToList(), pagingResponse);
-        //    return new OkObjectResult(response);
-        //}
-
         [HttpGet("projects")]
         [MapToApiVersion("2.0")]
         public ActionResult<ApiResponseV2<FssProjectResponse>> GetAll()
