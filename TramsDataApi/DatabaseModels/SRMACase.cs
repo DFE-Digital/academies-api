0d0eaa19c215c2d349f43e2a7f27849b5f47d9f9
Menol <menol.razeek@education.gov.uk>
2022-04-19 11:42:46 +0100

    SRMA Datamodes and migrations scripts

---
+++ TramsDataApi/DatabaseModels/SRMACase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TramsDataApi.DatabaseModels
+{
+    [Table("SRMACase", Schema = "sdd")]
+    public class SRMACase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public long? Urn { get; set; }
+        public int CaseId { get; set; }
+        public int StatusId { get; set; }
+        public int? CloseStatusId { get; set; }
+        public int? ReasonId { get; set; }
+        public DateTime DateOffered { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DateAccepted { get; set; }
+        public DateTime? StartDateOfVisit { get; set; }
+        public DateTime? EndDateOfVisit { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? ClosedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string? Notes { get; set; }
+
+        [ForeignKey(nameof(CaseId))]
+        public virtual ConcernsCase ConcernsCase { get; set; }
+
+        [ForeignKey(nameof(ReasonId))]
+        public virtual SRMAReason Reason { get; set; }
+    }
+}
---
+++ TramsDataApi/Migrations/TramsDb/20220414084000_SRMA_AddTables.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class SRMA_AddTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SRMAReason",
+                schema: "sdd",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Urn = table.Column<long>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SRMAReason", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SRMAStatus",
+                schema: "sdd",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Urn = table.Column<long>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SRMAStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SRMACase",
+                schema: "sdd",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Urn = table.Column<long>(nullable: true),
+                    CaseId = table.Column<int>(nullable: false),
+                    StatusId = table.Column<int>(nullable: false),
+                    CloseStatusId = table.Column<int>(nullable: true),
+                    ReasonId = table.Column<int>(nullable: true),
+                    DateOffered = table.Column<DateTime>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    DateAccepted = table.Column<DateTime>(nullable: true),
+                    StartDateOfVisit = table.Column<DateTime>(nullable: true),
+                    EndDateOfVisit = table.Column<DateTime>(nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    ClosedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SRMACase", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SRMACase_ConcernsCase_CaseId",
+                        column: x => x.CaseId,
+                        principalSchema: "sdd",
+                        principalTable: "ConcernsCase",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SRMACase_SRMAReason_ReasonId",
+                        column: x => x.ReasonId,
+                        principalSchema: "sdd",
+                        principalTable: "SRMAReason",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SRMACase_CaseId",
+                schema: "sdd",
+                table: "SRMACase",
+                column: "CaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SRMACase_ReasonId",
+                schema: "sdd",
+                table: "SRMACase",
+                column: "ReasonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SRMACase",
+                schema: "sdd");
+
+            migrationBuilder.DropTable(
+                name: "SRMAStatus",
+                schema: "sdd");
+
+            migrationBuilder.DropTable(
+                name: "SRMAReason",
+                schema: "sdd");
+        }
+    }
+}
