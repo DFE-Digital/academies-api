ce58aeb3941947fd32b8fb5658db3373de68206f
Menol <menol.razeek@education.gov.uk>
2022-04-25 16:37:32 +0100

    all layers and tests required get srma by id endpoint + minor refactoring

--- TramsDataApi/Controllers/V2/SRMAController.cs
+++ TramsDataApi/Controllers/V2/SRMAController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TramsDataApi.RequestModels.CaseActions.SRMA;
 using TramsDataApi.ResponseModels;
@@ -15,24 +16,37 @@ namespace TramsDataApi.Controllers.V2
     public class SRMAController : Controller
     {
         private readonly ILogger<SRMAController> _logger;
-        private readonly IUseCase<CreateSRMARequest, CreateSRMAResponse> _createSRMAUseCase;
+        private readonly IUseCase<CreateSRMARequest, SRMAResponse> _createSRMAUseCase;
+        private readonly IUseCase<int, ICollection<SRMAResponse>> _getSRMAsByCaseIdUseCase;
 
         public SRMAController(
             ILogger<SRMAController> logger,
-            IUseCase<CreateSRMARequest, CreateSRMAResponse> createSRMAUseCase)
+            IUseCase<CreateSRMARequest, SRMAResponse> createSRMAUseCase,
+            IUseCase<int, ICollection<SRMAResponse>> getSRMAsByCaseIdUseCase)
         {
             _logger = logger;
             _createSRMAUseCase = createSRMAUseCase;
+            _getSRMAsByCaseIdUseCase = getSRMAsByCaseIdUseCase;
         }
 
         [HttpPost]
         [MapToApiVersion("2.0")]
-        public ActionResult<ApiSingleResponseV2<CreateSRMAResponse>> Create(CreateSRMARequest request)
+        public ActionResult<ApiSingleResponseV2<SRMAResponse>> Create(CreateSRMARequest request)
         {
             var createdSRMA = _createSRMAUseCase.Execute(request);
-            var response = new ApiSingleResponseV2<CreateSRMAResponse>(createdSRMA);
+            var response = new ApiSingleResponseV2<SRMAResponse>(createdSRMA);
 
             return new ObjectResult(response) { StatusCode = StatusCodes.Status201Created };
+        }
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public ActionResult<ApiSingleResponseV2<ICollection<SRMAResponse>>> GetSRMAByCaseId(int caseId)
+        {
+            var srmas = _getSRMAsByCaseIdUseCase.Execute(caseId);
+            var response = new ApiSingleResponseV2<ICollection<SRMAResponse>>(srmas);
+
+            return Ok(response);
         }
     }
 }
