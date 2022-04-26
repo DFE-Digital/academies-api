5d8e29df8d2e160778f479ac3e27ca68662c02cc
MartinWheelerMT <martin.wheeler@madetech.com>
2022-04-26 16:48:41 +0100

    Rename SchoolCapacity fields in A2BApplicationApplyingSchool.cs to correct name ProjectedPupilNumbers so to avoid confusion with capacities. Created Migration for these changes Update test schema.js

---
+++ TramsDataApi/Migrations/TramsDb/20220426154014_RenameCapacityFieldsInA2BApplicationApplyingSchool.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class RenameCapacityFieldsInA2BApplicationApplyingSchool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SchoolCapacityYear1",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                newName: "ProjectedPupilNumbersYear1");
+
+            migrationBuilder.RenameColumn(
+                name: "SchoolCapacityYear2",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                newName: "ProjectedPupilNumbersYear2");
+
+            migrationBuilder.RenameColumn(
+                name: "SchoolCapacityYear3",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                newName: "ProjectedPupilNumbersYear3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProjectedPupilNumbersYear1",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                newName: "SchoolCapacityYear1");
+
+            migrationBuilder.RenameColumn(
+                name: "ProjectedPupilNumbersYear2",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                newName: "SchoolCapacityYear2");
+            
+            migrationBuilder.RenameColumn(
+                name: "ProjectedPupilNumbersYear1",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                newName: "SchoolCapacityYear2");
+        }
+    }
+}
