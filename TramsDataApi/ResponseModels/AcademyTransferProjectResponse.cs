3b4dd87dac08ebda20dd3028a98adaeb024cfd30
acarbutt <30597375+acarbutt@users.noreply.github.com>
2022-05-17 11:07:02 +0100

    Remove columns from Request and Response models (#191)

--- TramsDataApi/ResponseModels/AcademyTransferProjectResponse.cs
+++ TramsDataApi/ResponseModels/AcademyTransferProjectResponse.cs
@@ -19,11 +19,5 @@ namespace TramsDataApi.ResponseModels
         public AcademyTransferProjectGeneralInformationResponse GeneralInformation { get; set; }
         public string State { get; set; }
         public string Status { get; set; }
-        public string AcademyPerformanceAdditionalInformation { get; set; }
-        public string PupilNumbersAdditionalInformation { get; set; }
-        public string LatestOfstedJudgementAdditionalInformation { get; set; }
-        public string KeyStage2PerformanceAdditionalInformation { get; set; }
-        public string KeyStage4PerformanceAdditionalInformation { get; set; }
-        public string KeyStage5PerformanceAdditionalInformation { get; set; }
     }
 }
