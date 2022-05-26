6777c0bf4f51ccd7afdc89e5d8e03739a07a8985
Catherine Lawlor <55924986+CathLass@users.noreply.github.com>
2022-05-26 12:02:32 +0100

    Added TrustName to A2BApplication (#196)
    Added the trust name to the A2BApplication table and API response.

---
+++ TramsDataApi/Migrations/TramsDb/20220525133627_TrustNameAddedToA2BApplication.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class TrustNameAddedToA2BApplication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrustName",
+                schema: "sdd",
+                table: "A2BApplication",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrustName",
+                schema: "sdd",
+                table: "A2BApplication");
+        }
+    }
+}
