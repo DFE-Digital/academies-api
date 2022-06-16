35ccd229da8bd10b2e6ff0646e51e834536d83f8
MartinWheelerMT <martin.wheeler@madetech.com>
2022-06-16 11:54:35 +0100

    refactor of integration test add null return from GetAcademyConversionProject

--- TramsDataApi.Test/Controllers/AcademyConversionProjectsControllerTests.cs
+++ TramsDataApi.Test/Controllers/AcademyConversionProjectsControllerTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FizzWare.NBuilder;
 using Microsoft.Extensions.Logging;
--- TramsDataApi/UseCases/GetAcademyConversionProjects.cs
+++ TramsDataApi/UseCases/GetAcademyConversionProjects.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using TramsDataApi.Factories;
 using TramsDataApi.Gateways;
 using TramsDataApi.ResponseModels.AcademyConversionProject;
