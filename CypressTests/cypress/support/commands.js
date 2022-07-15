b56b63fee9477f49cb0267357839c821c7622555
Lisa Sau <lisasau85@gmail.com>
2022-07-15 11:01:15 +0100

    Task 100103:  Ordering of projects
    - Fetches & sorts project dates by random
    - Asserts that first project has most recent timestamp

--- CypressTests/cypress/support/commands.js
+++ CypressTests/cypress/support/commands.js
@@ -31,3 +31,16 @@ import { validateSchema } from "./validate-schema-command";
 Cypress.Commands.add("validateSchema", validateSchema);
 
 
+Cypress.Commands.add('beData', () => {
+    const apiKey = Cypress.env('apiKey')
+    const url = Cypress.env('url')
+
+    cy.request({
+        method:'GET',
+        url: url + '/conversion-projects?count=4',
+        headers: {
+            ApiKey: apiKey,
+            "Content-type" : "application/json"
+         }
+    })
+})
