d5d851bc820673056da81053eacaf935338bfcfc
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2022-08-24 09:32:39 +0100

    Feature/103738 cd team casework functionality (#231)
    * Adding the get and update models and endpoints for team casework functionality.
    
    * controller unit tests
    
    * Added model and migration for getting concerns team casework selected users
    
    * storing team casework selections
    
    * spint seleted team members into a team and tem member object hierachy
    
    * updated gateway to remove team members when no longer in team
    
    * refactorings to take into account concerns casework teams and team members
    
    * refactoring to tidy up file names and fix orphand concerns team members

---
+++ TramsDataApi/Gateways/ConcernsTeamCaseworkGateway.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TramsDataApi.DatabaseModels;
+using TramsDataApi.DatabaseModels.Concerns.TeamCasework;
+using static Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions;
+
+namespace TramsDataApi.Gateways
+{
+    public class ConcernsTeamCaseworkGateway : IConcernsTeamCaseworkGateway
+    {
+        private readonly TramsDbContext _tramsDbContext;
+
+        public ConcernsTeamCaseworkGateway(TramsDbContext tramsDbContext)
+        {
+            _tramsDbContext = tramsDbContext ?? throw new ArgumentNullException(nameof(tramsDbContext));
+        }
+
+        public async Task AddCaseworkTeam(ConcernsCaseworkTeam team, CancellationToken cancellationToken)
+        {
+            _ = team ?? throw new ArgumentNullException(nameof(team));            
+            _ = team.TeamMembers ?? throw new ArgumentNullException(nameof(team.TeamMembers));
+            
+            _tramsDbContext.ConcernsTeamCaseworkTeam.Add(team);
+            await _tramsDbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<ConcernsCaseworkTeam> GetByOwnerId(string ownerId, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(ownerId))
+            {
+                throw new ArgumentNullException(nameof(ownerId));
+            }
+
+            var team = await _tramsDbContext.ConcernsTeamCaseworkTeam
+                .Include(t => t.TeamMembers)
+                .FirstOrDefaultAsync(x => x.Id == ownerId);
+
+            return team;
+        }
+
+        public async Task UpdateCaseworkTeam(ConcernsCaseworkTeam team, CancellationToken cancellationToken)
+        {
+            _ = team ?? throw new ArgumentNullException(nameof(team));
+            if (string.IsNullOrWhiteSpace(team.Id))
+            {
+                throw new ArgumentNullException(nameof(team.Id));
+            }
+            _ = team.TeamMembers ?? throw new ArgumentNullException(nameof(team.TeamMembers));
+            
+            _tramsDbContext.ConcernsTeamCaseworkTeam.Update(team);
+            await _tramsDbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ TramsDataApi/UseCases/GetConcernsCaseworkTeam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TramsDataApi.Gateways;
+using TramsDataApi.ResponseModels.Concerns.TeamCasework;
+
+namespace TramsDataApi.UseCases
+{
+    public class GetConcernsCaseworkTeam : IGetConcernsCaseworkTeam
+    {
+        private readonly IConcernsTeamCaseworkGateway _gateway;
+
+        public GetConcernsCaseworkTeam(IConcernsTeamCaseworkGateway gateway)
+        {
+            _gateway = gateway ?? throw new ArgumentNullException(nameof(gateway));
+        }
+
+        public async Task<ConcernsCaseworkTeamResponse> Execute(string ownerId, CancellationToken cancellationToken)
+        {
+            var record = await _gateway.GetByOwnerId(ownerId, cancellationToken);
+
+            if (record is null)
+            {
+                return null;
+            }
+            return new ConcernsCaseworkTeamResponse { OwnerId = ownerId, TeamMembers = record.TeamMembers.Select(x => x.TeamMember).ToArray() };
+        }
+    }
+}
