dce3b3b7d844dd5b85e318f9a6ab5ece8563bf15
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-10-05 11:12:40 +0100

    AB#107735 assign project - add migration for assigned user

---
+++ TramsDataApi/Migrations/TramsDb/20221005100254_Add_assigneduser_to_conversionproject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class Add_assigneduser_to_conversionproject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedUserEmailAddress",
+                schema: "sdd",
+                table: "AcademyConversionProject",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedUserFullName",
+                schema: "sdd",
+                table: "AcademyConversionProject",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AssignedUserId",
+                schema: "sdd",
+                table: "AcademyConversionProject",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedUserEmailAddress",
+                schema: "sdd",
+                table: "AcademyConversionProject");
+
+            migrationBuilder.DropColumn(
+                name: "AssignedUserFullName",
+                schema: "sdd",
+                table: "AcademyConversionProject");
+
+            migrationBuilder.DropColumn(
+                name: "AssignedUserId",
+                schema: "sdd",
+                table: "AcademyConversionProject");
+        }
+    }
+}
