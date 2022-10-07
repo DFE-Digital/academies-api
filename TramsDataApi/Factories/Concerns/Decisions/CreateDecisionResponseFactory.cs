7de76c89845ea47fd63d6bd6a83d8e1ec924867e
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2022-10-07 14:51:56 +0100

    removed unneccessary code

--- TramsDataApi/Factories/Concerns/Decisions/CreateDecisionResponseFactory.cs
+++ TramsDataApi/Factories/Concerns/Decisions/CreateDecisionResponseFactory.cs
@@ -6,13 +6,6 @@ namespace TramsDataApi.Factories.Concerns.Decisions
     [Obsolete("This is planned to be moved into the Concerns Casework API. If it is accessed by other APIs, please let the Concerns team know.")]
     public class CreateDecisionResponseFactory : ICreateDecisionResponseFactory
     {
-        public CreateDecisionResponse Create(int concernsCaseUrn, int decisionId)
-        {
-            _ = concernsCaseUrn <= 0 ? throw new ArgumentOutOfRangeException(nameof(concernsCaseUrn)) : concernsCaseUrn;
-            _ = decisionId <= 0 ? throw new ArgumentOutOfRangeException(nameof(decisionId)) : decisionId;
-
-
-            return new CreateDecisionResponse(concernsCaseUrn, decisionId);
-        }
+        public CreateDecisionResponse Create(int concernsCaseUrn, int decisionId) => new CreateDecisionResponse(concernsCaseUrn, decisionId);
     }
 }
