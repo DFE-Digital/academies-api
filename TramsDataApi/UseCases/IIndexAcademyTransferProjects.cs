bae3485148263328cf3b2e5f33102787c6a05cb9
WILDE <Tim.WILDE@EDUCATION.GOV.UK>
2022-11-01 15:21:50 +0000

    Provide the total record count as well as the page of results.

--- TramsDataApi/UseCases/IIndexAcademyTransferProjects.cs
+++ TramsDataApi/UseCases/IIndexAcademyTransferProjects.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TramsDataApi.RequestModels;
 using TramsDataApi.ResponseModels.AcademyTransferProject;
@@ -6,6 +7,6 @@ namespace TramsDataApi.UseCases
 {
     public interface IIndexAcademyTransferProjects
     {
-        public IList<AcademyTransferProjectSummaryResponse> Execute(int page);
+        public Tuple<IList<AcademyTransferProjectSummaryResponse>, int> Execute(int page);
     }
 }
--- TramsDataApi/UseCases/IndexAcademyTransferProjects.cs
+++ TramsDataApi/UseCases/IndexAcademyTransferProjects.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 using TramsDataApi.DatabaseModels;
 using TramsDataApi.Gateways;
 using TramsDataApi.ResponseModels;
@@ -20,39 +20,42 @@ namespace TramsDataApi.UseCases
             _trustGateway = trustGateway;
         }
 
-        public IList<AcademyTransferProjectSummaryResponse> Execute(int page)
+        public Tuple<IList<AcademyTransferProjectSummaryResponse>, int> Execute(int page)
         {
-            var listOfAcademyTransferProjects = _academyTransferProjectGateway.IndexAcademyTransferProjects(page);
+           Tuple<IList<AcademyTransferProjects>, int> listOfAcademyTransferProjects =
+              _academyTransferProjectGateway.IndexAcademyTransferProjects(page);
 
-            return listOfAcademyTransferProjects.ToList().Select(atp =>
-            {
-                var outgoingGroup = _trustGateway.GetGroupByUkPrn(atp.OutgoingTrustUkprn);
-                return new AcademyTransferProjectSummaryResponse()
-                {
+           return Tuple.Create<IList<AcademyTransferProjectSummaryResponse>, int>(
+              listOfAcademyTransferProjects.Item1.ToList().Select(atp =>
+              {
+                 Group outgoingGroup = _trustGateway.GetGroupByUkPrn(atp.OutgoingTrustUkprn);
+                 return new AcademyTransferProjectSummaryResponse
+                 {
                     ProjectUrn = atp.Urn.ToString(),
                     ProjectReference = atp.ProjectReference,
                     OutgoingTrustUkprn = atp.OutgoingTrustUkprn,
                     OutgoingTrustName = outgoingGroup.GroupName,
                     OutgoingTrustLeadRscRegion =
-                        _trustGateway.GetIfdTrustByGroupId(outgoingGroup.GroupId).LeadRscRegion,
+                       _trustGateway.GetIfdTrustByGroupId(outgoingGroup.GroupId).LeadRscRegion,
                     TransferringAcademies = atp.TransferringAcademies.Select(ta =>
                     {
-                        var group = _trustGateway.GetGroupByUkPrn(ta.IncomingTrustUkprn);
-                        return new TransferringAcademiesResponse
-                        {
-                            OutgoingAcademyUkprn = ta.OutgoingAcademyUkprn,
-                            IncomingTrustUkprn = ta.IncomingTrustUkprn,
-                            IncomingTrustName = group.GroupName,
-                            IncomingTrustLeadRscRegion = _trustGateway.GetIfdTrustByGroupId(group.GroupId).LeadRscRegion,
-                            PupilNumbersAdditionalInformation = ta.PupilNumbersAdditionalInformation,
-                            LatestOfstedReportAdditionalInformation = ta.LatestOfstedReportAdditionalInformation,
-                            KeyStage2PerformanceAdditionalInformation = ta.KeyStage2PerformanceAdditionalInformation,
-                            KeyStage4PerformanceAdditionalInformation = ta.KeyStage4PerformanceAdditionalInformation,
-                            KeyStage5PerformanceAdditionalInformation = ta.KeyStage5PerformanceAdditionalInformation
-                        };
+                       Group group = _trustGateway.GetGroupByUkPrn(ta.IncomingTrustUkprn);
+                       return new TransferringAcademiesResponse
+                       {
+                          OutgoingAcademyUkprn = ta.OutgoingAcademyUkprn,
+                          IncomingTrustUkprn = ta.IncomingTrustUkprn,
+                          IncomingTrustName = group.GroupName,
+                          IncomingTrustLeadRscRegion = _trustGateway.GetIfdTrustByGroupId(group.GroupId).LeadRscRegion,
+                          PupilNumbersAdditionalInformation = ta.PupilNumbersAdditionalInformation,
+                          LatestOfstedReportAdditionalInformation = ta.LatestOfstedReportAdditionalInformation,
+                          KeyStage2PerformanceAdditionalInformation = ta.KeyStage2PerformanceAdditionalInformation,
+                          KeyStage4PerformanceAdditionalInformation = ta.KeyStage4PerformanceAdditionalInformation,
+                          KeyStage5PerformanceAdditionalInformation = ta.KeyStage5PerformanceAdditionalInformation
+                       };
                     }).ToList()
-                };
-            }).ToList();
+                 };
+              }).ToList(),
+              listOfAcademyTransferProjects.Item2);
         }
     }
 }
