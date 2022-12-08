9900ce27e83f5c4f910466ce505efd37f28e7864
mark robinson <mark.robinson@education.gov.uk>
2022-12-08 13:34:59 +0000

    added 2 missing cols from A2B school

---
+++ TramsDataApi/Migrations/TramsDb/20221208133423_sdd-newcols-datamigrationmk2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class sddnewcolsdatamigrationmk2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocalAuthorityName",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Urn",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocalAuthorityName",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool");
+
+            migrationBuilder.DropColumn(
+                name: "Urn",
+                schema: "sdd",
+                table: "A2BApplicationApplyingSchool");
+        }
+    }
+}
