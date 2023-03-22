114921ff7b2c223f67d47971518b49111e6ae9a1
Tim Wilde <tim.wilde@education.gov.uk>
2023-03-22 10:29:28 +0000

    Adds a CreatedOn column to the AcademyTransfersProject table for auditing use

---
+++ TramsDataApi/Migrations/TramsDb/20230322101755_project-created-on-date.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+   public partial class projectcreatedondate : Migration
+   {
+      protected override void Up(MigrationBuilder migrationBuilder)
+      {
+         migrationBuilder.AddColumn<DateTime>(
+            "CreatedOn",
+            schema: "sdd",
+            table: "AcademyTransferProjects",
+            nullable: false,
+            defaultValueSql: "GETDATE()");
+      }
+
+      protected override void Down(MigrationBuilder migrationBuilder)
+      {
+         migrationBuilder.DropColumn(
+            "CreatedOn",
+            schema: "sdd",
+            table: "AcademyTransferProjects");
+      }
+   }
+}
