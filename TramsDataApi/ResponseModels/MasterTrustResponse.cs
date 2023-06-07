f9635f05b1e0255defaf94e2d7c8ff849bf3318f
mikestock-nimble <mike.stock@nimbleapproach.com>
2023-06-07 08:36:01 +0100

    129186: removed the bulk api from v3 as concerns does not need it
    this is extra effort to test and we do not need the api, it can be added when it is required

--- TramsDataApi/ResponseModels/MasterTrustResponse.cs
+++ TramsDataApi/ResponseModels/MasterTrustResponse.cs
@@ -4,7 +4,7 @@ namespace TramsDataApi.ResponseModels
 {
     public class MasterTrustResponse
     {
-        public MasterTrustDataResponse MasterTrustData { get; set; }
+        public MasterTrustDataResponse TrustData { get; set; }
         public GIASDataResponse GiasData { get; set; }
         public List<EstablishmentResponse> Establishments { get; set; }
     }
