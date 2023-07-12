ce4079d49d3e0bb9757ddad1bfc1bfb974c9e863
plockwood <paul.lockwood@education.gov.uk>
2023-07-12 14:10:52 +0100

    Academy transfer project duplicate urn fix

---
+++ TramsDataApi/Migrations/TramsDb/20230712121010_remove-sequence-academy-transfer-project-urns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TramsDataApi.Migrations.TramsDb
+{
+    public partial class removesequenceacademytransferprojecturns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF EXISTS(SELECT 1 FROM sys.sequences WHERE name = 'AcademyTransferProjectUrns' and SCHEMA_NAME(schema_id) = 'dbo')
+                                     BEGIN
+                                       DROP SEQUENCE [dbo].[AcademyTransferProjectUrns]
+                                     END");
+
+            migrationBuilder.Sql(@"IF EXISTS(SELECT 1 FROM sys.sequences WHERE name = 'AcademyTransferProjectUrns' and SCHEMA_NAME(schema_id) = 'sdd')
+                                     BEGIN
+                                       DROP SEQUENCE [sdd].[AcademyTransferProjectUrns]
+                                     END");
+        }
+    }
+}
