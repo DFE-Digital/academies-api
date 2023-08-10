cc87aa1dd260fa6d1bc9a70c67a74751a4fd9c27
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-08-10 11:47:38 +0100

    Use correct process env vars for zap proxy

--- CypressTests/cypress/plugins/index.js
+++ CypressTests/cypress/plugins/index.js
@@ -27,7 +27,7 @@ module.exports = (on, config) => {
 
   // eslint-disable-next-line no-unused-vars
   on('after:run', async () => {
-    if(process.env.zap) {
+    if(process.env.ZAP) {
       await generateZapHTMLReport()
     }
   })
