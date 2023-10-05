8e92965769d7a172e6bca086efbd2c51d6014b9b
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2023-10-05 11:35:16 +0100

    Remove a2b

--- TramsDataApi/UseCases/IGetA2BContributor.cs
+++
@@ -1,9 +0,0 @@
-using TramsDataApi.ResponseModels.ApplyToBecome;
-
-namespace TramsDataApi.UseCases
-{
-    public interface IGetA2BContributor
-    {
-        A2BContributorResponse Execute(string contributorId);
-    }
-}
