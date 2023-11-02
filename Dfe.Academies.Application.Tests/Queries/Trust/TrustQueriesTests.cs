4a8fff408448bdb250d7efab3568611017cd8541
plockwood <paul.lockwood@education.gov.uk>
2023-11-02 17:44:33 +0000

    tests

--- Dfe.Academies.Application.Tests/Queries/Trust/TrustQueriesTests.cs
+++ Dfe.Academies.Application.Tests/Queries/Trust/TrustQueriesTests.cs
@@ -1,5 +1,8 @@
-﻿using Dfe.Academies.Application.Queries.Trust;
+﻿using AutoFixture;
+using Dfe.Academies.Application.Queries.Trust;
 using Dfe.Academies.Domain.Trust;
+using FluentAssertions;
+using Moq;
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -8,12 +11,23 @@ namespace Dfe.Academies.Application.Tests.Queries.Trust
 {
     public class TrustQueriesTests
     {
+        private Fixture _fixture;
+
+        public TrustQueriesTests()
+        {
+            _fixture = new Fixture();
+        }
+
         [Fact]
-        public async Task GetByUkprn_StateUnderTest_ExpectedBehavior()
+        public async Task GetByUkprn_MatchingTrustInRepository_ReturnsTrustDto()
         {
             // Arrange
-            var trustQueries = new TrustQueries(TODO);
-            string ukprn = null;
+            var trust = _fixture.Create<Domain.Trust.Trust?>();
+            var mockRepo = new Mock<ITrustRepository>();
+            string ukprn = "1010101";
+            mockRepo.Setup(x => x.GetTrustByUkprn(It.Is<string>(v => v == ukprn), It.IsAny<CancellationToken>())).Returns(Task.FromResult(trust));
+
+                var trustQueries = new TrustQueries(mockRepo.Object);
             CancellationToken cancellationToken = default(global::System.Threading.CancellationToken);
 
             // Act
@@ -22,14 +36,27 @@ namespace Dfe.Academies.Application.Tests.Queries.Trust
                 cancellationToken);
 
             // Assert
-            Assert.True(false);
+            result.Should().NotBeNull();
+            result.Name.Should().Be(trust.Name);
+            result.Address.Street.Should().Be(trust.AddressLine1);
+            result.Address.Additional.Should().Be(trust.AddressLine2);
+            result.Address.Locality.Should().Be(trust.AddressLine3);
+            result.Address.Town.Should().Be(trust.Town);
+            result.Address.Postcode.Should().Be(trust.Postcode);
+            result.Address.County.Should().Be(trust.County);
+            result.ReferenceNumber.Should().Be(trust.GroupID);
+            result.CompaniesHouseNumber.Should().Be(trust.CompaniesHouseNumber);
+            result.Ukprn.Should().Be(trust.UKPRN);
+
         }
 
         [Fact]
         public async Task Search_StateUnderTest_ExpectedBehavior()
         {
             // Arrange
-            var trustQueries = new TrustQueries(TODO);
+            var mockRepo = new Mock<ITrustRepository>();
+
+            var trustQueries = new TrustQueries(mockRepo.Object);
             int page = 0;
             int count = 0;
             string name = null;
@@ -51,10 +78,14 @@ namespace Dfe.Academies.Application.Tests.Queries.Trust
         }
 
         [Fact]
-        public async Task GetByUkprns_StateUnderTest_ExpectedBehavior()
+        public async Task GetByUkprns_TrustsReturnedFromRepo_ReturnsAListOfTrustDtos()
         {
             // Arrange
-            var trustQueries = new TrustQueries(TODO);
+            var trusts = _fixture.Create<List<Domain.Trust.Trust>>();
+            var mockRepo = new Mock<ITrustRepository>();
+            mockRepo.Setup(x => x.GetTrustsByUkprns(It.IsAny<string[]>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(trusts));
+
+            var trustQueries = new TrustQueries(mockRepo.Object);
             string[] ukprns = null;
             CancellationToken cancellationToken = default(global::System.Threading.CancellationToken);
 
@@ -64,7 +95,23 @@ namespace Dfe.Academies.Application.Tests.Queries.Trust
                 cancellationToken);
 
             // Assert
-            Assert.True(false);
+            result.Should().NotBeNull();
+            result.Count.Should().Be(trusts.Count);
+
+            foreach (var trust in result) {
+                var domainTrust = trusts.Single(x => x.UKPRN == trust.Ukprn);
+
+                trust.Name.Should().Be(domainTrust.Name);
+                trust.Address.Street.Should().Be(domainTrust.AddressLine1);
+                trust.Address.Additional.Should().Be(domainTrust.AddressLine2);
+                trust.Address.Locality.Should().Be(domainTrust.AddressLine3);
+                trust.Address.Town.Should().Be(domainTrust.Town);
+                trust.Address.Postcode.Should().Be(domainTrust.Postcode);
+                trust.Address.County.Should().Be(domainTrust.County);
+                trust.ReferenceNumber.Should().Be(domainTrust.GroupID);
+                trust.CompaniesHouseNumber.Should().Be(domainTrust.CompaniesHouseNumber);
+                trust.Ukprn.Should().Be(domainTrust.UKPRN);
+            }  
         }
     }
 }
