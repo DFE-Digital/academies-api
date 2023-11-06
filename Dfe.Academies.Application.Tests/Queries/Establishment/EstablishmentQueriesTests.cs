645b723e82899bb8028c006b9231a59d1599700a
plockwood <paul.lockwood@education.gov.uk>
2023-11-06 11:11:38 +0000

    added field mappings to unit tests

--- Dfe.Academies.Application.Tests/Queries/Establishment/EstablishmentQueriesTests.cs
+++ Dfe.Academies.Application.Tests/Queries/Establishment/EstablishmentQueriesTests.cs
@@ -6,6 +6,7 @@ using Dfe.Academies.Domain.Establishment;
 using Dfe.Academies.Domain.Trust;
 using FluentAssertions;
 using Moq;
+using System.Runtime.CompilerServices;
 
 namespace Dfe.Academies.Application.Tests.Queries.Establishment
 {
@@ -41,6 +42,7 @@ namespace Dfe.Academies.Application.Tests.Queries.Establishment
 
             // Assert
             result.Should().BeOfType(typeof(EstablishmentDto));
+            Assert.True(HasMappedCorrectly(result, establishment));
         }
 
         [Fact]
@@ -64,6 +66,7 @@ namespace Dfe.Academies.Application.Tests.Queries.Establishment
 
             // Assert
             result.Should().BeOfType(typeof(EstablishmentDto));
+            Assert.True(HasMappedCorrectly(result, establishment));
         }
 
         [Fact]
@@ -91,6 +94,10 @@ namespace Dfe.Academies.Application.Tests.Queries.Establishment
 
             // Assert
             result.Should().BeOfType(typeof((List<EstablishmentDto>, int)));
+            foreach (var establishmentDto in result.Item1) {
+                var establishment = establishments.Single(x => x.URN.ToString() == establishmentDto.Urn);
+                Assert.True(HasMappedCorrectly(establishmentDto, establishment));
+            }
         }
 
         [Fact]
@@ -113,6 +120,7 @@ namespace Dfe.Academies.Application.Tests.Queries.Establishment
 
             // Assert
             result.Should().BeAssignableTo(typeof(IEnumerable<int>));
+            result.Should().BeEquivalentTo(establishmentUrns);
 
         }
 
@@ -134,6 +142,66 @@ namespace Dfe.Academies.Application.Tests.Queries.Establishment
 
             // Assert
             result.Should().BeOfType(typeof(List<EstablishmentDto>));
+            foreach (var establishmentDto in result)
+            {
+                var establishment = establishments.Single(x => x.URN.ToString() == establishmentDto.Urn);
+                Assert.True(HasMappedCorrectly(establishmentDto, establishment));
+            }
         }
+
+        private bool HasMappedCorrectly(EstablishmentDto dto, Domain.Establishment.Establishment establishment)
+        {
+            return (
+                dto.Name == establishment.EstablishmentName &&
+                dto.Urn == establishment.URN.ToString() &&
+
+                dto.OfstedRating == establishment.OfstedRating &&
+                dto.OfstedLastInspection == establishment.OfstedLastInspection &&
+                dto.StatutoryLowAge == establishment.StatutoryLowAge &&
+                dto.StatutoryHighAge == establishment.StatutoryHighAge &&
+                dto.SchoolCapacity == establishment.SchoolCapacity &&
+                dto.Pfi == establishment.IfdPipeline?.DeliveryProcessPFI &&
+                dto.EstablishmentNumber == establishment.EstablishmentNumber.ToString() &&
+                dto.Pan == establishment.IfdPipeline?.DeliveryProcessPAN &&
+                dto.Deficit == establishment.IfdPipeline?.ProjectTemplateInformationDeficit &&
+                dto.ViabilityIssue == establishment.IfdPipeline?.ProjectTemplateInformationViabilityIssue &&
+
+                dto.LocalAuthorityCode == establishment.LocalAuthority?.Code &&
+                dto.LocalAuthorityName == establishment.LocalAuthority?.Name &&
+
+                dto.Diocese.Name == establishment.Diocese &&
+
+                dto.EstablishmentType.Name == establishment.EstablishmentType?.Name &&
+                dto.EstablishmentType.Code == establishment.EstablishmentType?.Code &&
+
+                dto.Gor.Name == establishment.GORregion &&
+
+                dto.PhaseOfEducation.Name == establishment.PhaseOfEducation &&
+
+                dto.ReligiousCharacter.Name == establishment.ReligiousCharacter &&
+
+                dto.ParliamentaryConstituency.Name == establishment.ParliamentaryConstituency &&
+
+                dto.Census.NumberOfPupils == establishment.NumberOfPupils &&
+                dto.Census.PercentageFsm == establishment.PercentageFSM &&
+
+                dto.MISEstablishment.DateOfLatestSection8Inspection == establishment.DateOfLatestShortInspection?.ToString() &&
+                dto.MISEstablishment.InspectionEndDate == establishment.InspectionEndDate?.ToString() &&
+                dto.MISEstablishment.OverallEffectiveness == establishment.OverallEffectiveness?.ToString() &&
+                dto.MISEstablishment.QualityOfEducation == establishment.QualityOfEducation?.ToString() &&
+                dto.MISEstablishment.BehaviourAndAttitudes == establishment.BehaviourAndAttitudes?.ToString() &&
+                dto.MISEstablishment.PersonalDevelopment == establishment.PersonalDevelopment?.ToString() &&
+                dto.MISEstablishment.EffectivenessOfLeadershipAndManagement == establishment.EffectivenessOfLeadershipAndManagement?.ToString() &&
+                dto.MISEstablishment.EarlyYearsProvision == establishment.EarlyYearsProvisionWhereApplicable?.ToString() &&
+                dto.MISEstablishment.SixthFormProvision == establishment.SixthFormProvisionWhereApplicable?.ToString() &&
+
+                dto.Address.Street == establishment.AddressLine1 &&
+                dto.Address.Town == establishment.Town &&
+                dto.Address.Postcode == establishment.Postcode &&
+                dto.Address.County == establishment.County &&
+                dto.Address.Additional == establishment.AddressLine2
+                );
+        }
+
     }
 }
