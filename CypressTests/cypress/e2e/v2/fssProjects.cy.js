e33a6b202a7971f2390050e9affe97ca49c2a892
Chris Sherlock <99017916+cshnimble@users.noreply.github.com>
2024-01-12 11:40:14 +0000

    Cypress: extend fss endpoint test to check for data and a set of keys in response (#444)

--- CypressTests/cypress/e2e/v2/fssProjects.cy.js
+++ CypressTests/cypress/e2e/v2/fssProjects.cy.js
@@ -4,13 +4,18 @@ describe("Free Schools Store endpoint", () => {
   const apiKey = Cypress.env('apiKey')
   const baseUrlV2 = `${Cypress.env('url')}/v2`
   
-  it("Should return a valid 200 response", () => {
+  it("Should return a valid response with data", () => {
     cy.api({
       url: `${baseUrlV2}/fss/projects`,
       headers: {
         ApiKey: apiKey,
       }
-    }).its('status').should('eq', 200)
+    })
+    .then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.data).to.have.lengthOf.at.least(1)
+      expect(response.body.data[0]).to.include.all.keys('localAuthority', 'projectId', 'projectStatus', 'trustId', 'trustName', 'urn')
+    })
   })
 
   it("Should return a valid 401 response when omitting API key", () => {
