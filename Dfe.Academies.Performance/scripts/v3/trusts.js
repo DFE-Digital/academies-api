4ecb249a3b66db8047e3bdae46da7355fc0be24a
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2024-02-08 11:02:21 +0000

    Update perf tests for constant benchmark load

--- Dfe.Academies.Performance/scripts/v3/trusts.js
+++ Dfe.Academies.Performance/scripts/v3/trusts.js
@@ -1,10 +1,21 @@
 import http from 'k6/http'
-import { check, sleep } from 'k6'
+import { sleep } from 'k6'
 import { isStatus200, getHeaders } from '../utils/utils.js'
 
 export const options = {
-    vus: 20,
-    duration: '30s'
+  thresholds: {
+    http_req_failed: ['rate<0.01'],   // Failure rate less than 1%
+    http_req_duration: ['p(95)<1000'] // Requests take <1s at the 95th percentile
+  },
+  scenarios: {
+    constant_load: {
+      executor: 'constant-arrival-rate',
+      duration: '10s',
+      preAllocatedVUs: 100,
+      rate: 10,
+      timeUnit: '1s'
+    }
+  }
 }
 
 const baseUrl = `${__ENV.BASE_URL}/v3`
@@ -22,7 +33,7 @@ export default function () {
 
 function getTrustByUkPrn(ukprn) {
   const res = http.get(`${baseUrl}/trust/${ukprn}`, {
-      headers: getHeaders()
+    headers: getHeaders()
   })
 
   isStatus200(res)
@@ -30,15 +41,15 @@ function getTrustByUkPrn(ukprn) {
 
 function searchTrustByName(name) {
   const res = http.get(`${baseUrl}/trusts?groupName=${name}&page=1&count=10`, {
-      headers: getHeaders()
+    headers: getHeaders()
   })
 
-  isStatus200(res);
+  isStatus200(res)
 }
 
 function searchTrustByUkPrn(ukprn) {
   const res = http.get(`${baseUrl}/trusts?ukPrn=${ukprn}&page=1&count=10`, {
-      headers: getHeaders()
+    headers: getHeaders()
   })
 
   isStatus200(res)
