cad3c473526924c621720f8e0108fee7d733a60b
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-03-06 14:03:11 +0000

    Updated the tests to use inline data and reduces duplication

--- TramsDataApi.Test/Integration/V4/IdentifiersV4IntegrationTests.cs
+++ TramsDataApi.Test/Integration/V4/IdentifiersV4IntegrationTests.cs
@@ -5,9 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-using AutoFixture;
 using Dfe.Academies.Application.Trust;
-using Dfe.Academies.Contracts.V4.Trusts;
 using Dfe.Academies.Domain.Trust;
 using FluentAssertions;
 using TramsDataApi.Test.Fixtures;
@@ -19,7 +17,6 @@ namespace TramsDataApi.Test.Integration.V4;
 public class IdentifiersV4IntegrationTests
 {
     private readonly HttpClient _client;
-    private static readonly Fixture _autoFixture = new Fixture();
     private readonly ApiTestFixture _apiFixture;
 
     private readonly string _apiUrlPrefix = "/v4";
@@ -30,8 +27,11 @@ public class IdentifiersV4IntegrationTests
         _client = fixture.Client;
     }
     
-    [Fact]
-    public async Task Get_TrustIdentifiersByUkPrn_AndTrustExists_Returns_Ok()
+    [Theory]
+    [InlineData(IdTypes.GroupID)]
+    [InlineData(IdTypes.UKPRN)]
+    [InlineData(IdTypes.GroupUID)]
+    public async Task Get_TrustIdentifiers_AndTrustExists_Returns_Ok(IdTypes idType)
     {
         using var context = _apiFixture.GetMstrContext();
 
@@ -42,7 +42,15 @@ public class IdentifiersV4IntegrationTests
 
         var selectedTrust = trustData.First();
 
-        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/{selectedTrust.UKPRN}");
+        var identifier = idType switch
+        {
+            IdTypes.GroupID => selectedTrust.GroupID,
+            IdTypes.UKPRN => selectedTrust.UKPRN,
+            IdTypes.GroupUID => selectedTrust.GroupUID,
+            _ => throw new ArgumentOutOfRangeException(nameof(idType), idType, null)
+        };
+
+        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/{identifier}");
         trustResponse.StatusCode.Should().Be(HttpStatusCode.OK);
 
         var trustContent = await trustResponse.Content.ReadFromJsonAsync<TrustIdentifiers[]>();
@@ -50,99 +58,30 @@ public class IdentifiersV4IntegrationTests
         AssertIdentifierResponse(trustContent.First(), selectedTrust);
     }
     
-    [Fact]
-    public async Task Get_TrustIdentifiersByUID_AndTrustExists_Returns_Ok()
+    [Theory]
+    [InlineData(IdTypes.GroupID)]
+    [InlineData(IdTypes.UKPRN)]
+    [InlineData(IdTypes.GroupUID)]
+    public async Task Get_TrustIdentifiers_AndDuplicateTrustsExists_Returns_Ok(IdTypes idType)
     {
         using var context = _apiFixture.GetMstrContext();
 
-        var trustData = BuildSmallTrustSet();
+        var trustData = BuildSmallDuplicateTrustSet(idType);
 
         context.Trusts.AddRange(trustData);
         context.SaveChanges();
 
         var selectedTrust = trustData.First();
+        
+        var identifier = idType switch
+        {
+            IdTypes.GroupID => "GroupID",
+            IdTypes.UKPRN => "UKPRN",
+            IdTypes.GroupUID => "GroupUID",
+            _ => throw new ArgumentOutOfRangeException(nameof(idType), idType, null)
+        };
 
-        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/{selectedTrust.GroupUID}");
-        trustResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-
-        var trustContent = await trustResponse.Content.ReadFromJsonAsync<TrustIdentifiers[]>();
-        trustContent.Length.Should().Be(1);
-        AssertIdentifierResponse(trustContent.First(), selectedTrust);
-    }
-    
-    [Fact]
-    public async Task Get_TrustIdentifiersByGroupID_AndTrustExists_Returns_Ok()
-    {
-        using var context = _apiFixture.GetMstrContext();
-
-        var trustData = BuildSmallTrustSet();
-
-        context.Trusts.AddRange(trustData);
-        context.SaveChanges();
-
-        var selectedTrust = trustData.First();
-
-        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/{selectedTrust.GroupID}");
-        trustResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-
-        var trustContent = await trustResponse.Content.ReadFromJsonAsync<TrustIdentifiers[]>();
-        trustContent.Length.Should().Be(1);
-        AssertIdentifierResponse(trustContent.First(), selectedTrust);
-    }
-    
-    [Fact]
-    public async Task Get_TrustIdentifiersByUKPRN_AndDuplicateTrustsExists_Returns_Ok()
-    {
-        using var context = _apiFixture.GetMstrContext();
-
-        var trustData = BuildSmallDuplicateTrustSet(IdTypes.UKPRN);
-
-        context.Trusts.AddRange(trustData);
-        context.SaveChanges();
-
-        var selectedTrust = trustData.First();
-
-        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/UKPRN");
-        trustResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-
-        var trustContent = await trustResponse.Content.ReadFromJsonAsync<TrustIdentifiers[]>();
-        trustContent.Length.Should().Be(3);
-        AssertIdentifierResponse(trustContent.First(), selectedTrust);
-    }
-    
-    [Fact]
-    public async Task Get_TrustIdentifiersByGroupUID_AndDuplicateTrustsExists_Returns_Ok()
-    {
-        using var context = _apiFixture.GetMstrContext();
-
-        var trustData = BuildSmallDuplicateTrustSet(IdTypes.GroupUID);
-
-        context.Trusts.AddRange(trustData);
-        context.SaveChanges();
-
-        var selectedTrust = trustData.First();
-
-        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/GroupUID");
-        trustResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-
-        var trustContent = await trustResponse.Content.ReadFromJsonAsync<TrustIdentifiers[]>();
-        trustContent.Length.Should().Be(3);
-        AssertIdentifierResponse(trustContent.First(), selectedTrust);
-    }
-    
-    [Fact]
-    public async Task Get_TrustIdentifiersByGroupID_AndDuplicateTrustsExists_Returns_Ok()
-    {
-        using var context = _apiFixture.GetMstrContext();
-
-        var trustData = BuildSmallDuplicateTrustSet(IdTypes.GroupID);
-
-        context.Trusts.AddRange(trustData);
-        context.SaveChanges();
-
-        var selectedTrust = trustData.First();
-
-        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/GroupID");
+        var trustResponse = await _client.GetAsync($"{_apiUrlPrefix}/identifier/{identifier}");
         trustResponse.StatusCode.Should().Be(HttpStatusCode.OK);
 
         var trustContent = await trustResponse.Content.ReadFromJsonAsync<TrustIdentifiers[]>();
@@ -162,8 +101,8 @@ public class IdentifiersV4IntegrationTests
 
         return result;
     }
-    
-    private enum IdTypes { GroupID, UKPRN, GroupUID } 
+
+    public enum IdTypes { GroupID, UKPRN, GroupUID } 
     
     private static List<Trust> BuildSmallDuplicateTrustSet(IdTypes id)
     {
@@ -193,7 +132,7 @@ public class IdentifiersV4IntegrationTests
     
     private static void AssertIdentifierResponse(TrustIdentifiers actual, Trust expected)
     {
-        actual.TR.Should().Be(expected.GroupID);
+        actual.TrustReference.Should().Be(expected.GroupID);
         actual.UKPRN.Should().Be(expected.UKPRN);
         actual.UID.Should().Be(expected.GroupUID);
     }
