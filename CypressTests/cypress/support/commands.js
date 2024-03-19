177bf55034f4a9766c4973a978a3af973276b629
Chris Sherlock <99017916+cshnimble@users.noreply.github.com>
2024-03-19 10:23:31 +0000

    Remove unused Cypress schema command & packages (#462)
    * Remove unused schema command and package
    
    * Remove extra unused packages

--- CypressTests/cypress/support/commands.js
+++ CypressTests/cypress/support/commands.js
@@ -23,9 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-
-
-import { validateSchema } from "./validate-schema-command"
-
-Cypress.Commands.add("validateSchema", validateSchema)
