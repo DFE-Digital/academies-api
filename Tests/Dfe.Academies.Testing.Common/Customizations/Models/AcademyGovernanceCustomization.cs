6b9b0ccedb6be1c04e95d2ef53d6c130ac0a0832
Farshad DASHTI <78855469+FrostyApeOne@users.noreply.github.com>
2024-09-18 09:27:35 +0100

    Improving unit tests

--- Tests/Dfe.Academies.Testing.Common/Customizations/Models/AcademyGovernanceCustomization.cs
+++ Tests/Dfe.Academies.Testing.Common/Customizations/Models/AcademyGovernanceCustomization.cs
@@ -5,16 +5,24 @@ namespace Dfe.Academies.Testing.Common.Customizations.Models
 {
     public class AcademyGovernanceCustomization : ICustomization
     {
+        public string? FirstName { get; set; } = "John";
+        public string? LastName { get; set; } = "Doe";
+        public string? Email { get; set; } = "john.doe@example.com";
+        public string? DisplayName { get; set; } = "John Doe";
+        public string? DisplayNameWithTitle { get; set; } = "Mr. John Doe";
+        public List<string> Roles { get; set; } = ["MP"];
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
         public void Customize(IFixture fixture)
         {
             fixture.Customize<AcademyGovernance>(composer => composer
-                .With(x => x.FirstName, "John")
-                .With(x => x.LastName, "Doe")
-                .With(x => x.Email, "john.doe@example.com")
-                .With(x => x.DisplayName, "John Doe")
-                .With(x => x.DisplayNameWithTitle, "Mr. John Doe")
-                .With(x => x.Roles, new List<string> { "MP" })
-                .With(x => x.UpdatedAt, DateTime.Now));
+                .With(x => x.FirstName, FirstName)
+                .With(x => x.LastName, LastName)
+                .With(x => x.Email, Email)
+                .With(x => x.DisplayName, DisplayName)
+                .With(x => x.DisplayNameWithTitle, DisplayNameWithTitle)
+                .With(x => x.Roles, Roles)
+                .With(x => x.UpdatedAt, UpdatedAt));
         }
     }
 }
