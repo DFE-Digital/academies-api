1112337abd3e2052e567f1d42e0670c05ebdccc3
Farshad DASHTI <78855469+FrostyApeOne@users.noreply.github.com>
2025-01-14 15:28:46 +0000

    WIP

--- Dfe.Academies.Application/Common/Models/AcademyGovernance.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dfe.Academies.Application.Common.Models
-{
-    public class AcademyGovernance : Person
-    {
-        public string? UKPRN { get; set; }
-        public int? URN { get; set; }
-    }
-}
--- Dfe.Academies.Application/Common/Models/TrustGovernance.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dfe.Academies.Application.Common.Models
-{
-    public class TrustGovernance : Person
-    {
-        public string? UKPRN { get; set; }
-        public string? TRN { get; set; }
-    }
-}
