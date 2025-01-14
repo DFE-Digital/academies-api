114c5669f02f7e956c60f85e5da5edf5913ce7ab
Farshad DASHTI <78855469+FrostyApeOne@users.noreply.github.com>
2025-01-14 15:28:46 +0000

    Initial migration

--- Dfe.Academies.Api.Infrastructure/InfrastructureServiceCollectionExtensions.cs
+++ Dfe.Academies.Api.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -1,13 +1,9 @@
 ﻿using Dfe.Academies.Application.Common.Interfaces;
-using Dfe.Academies.Domain.Constituencies;
+using Dfe.Academies.Domain.Interfaces.Repositories;
 using Dfe.Academies.Infrastructure;
 using Dfe.Academies.Infrastructure.Repositories;
-using Dfe.Academies.Infrastructure.Security.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Dfe.Academies.Domain.Interfaces.Repositories;
-using Dfe.Academies.Infrastructure.QueryServices;
-using Dfe.Academies.Domain.ValueObjects;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -35,41 +31,6 @@ namespace Microsoft.Extensions.DependencyInjection
                 options.UseSqlServer(connectionString));
 
             AddInfrastructureHealthChecks(services);
-
-            return services;
-        }
-
-        public static IServiceCollection AddPersonsApiInfrastructureDependencyGroup(
-            this IServiceCollection services, IConfiguration config)
-        {
-            //Repos
-            services.AddScoped<ITrustRepository, TrustRepository>();
-            services.AddScoped<IEstablishmentRepository, EstablishmentRepository>();
-            services.AddScoped<IConstituencyRepository, ConstituencyRepository>();
-
-            // Query Services
-            services.AddScoped<IEstablishmentQueryService, EstablishmentQueryService>();
-            services.AddScoped<ITrustQueryService, TrustQueryService>();
-
-            //Cache service
-            services.AddServiceCaching(config);
-
-            //Db
-            var connectionString = config.GetConnectionString("DefaultConnection");
-
-            services.AddDbContext<MstrContext>(options =>
-                options.UseSqlServer(connectionString));
-
-            services.AddDbContext<MopContext>(options =>
-                options.UseSqlServer(connectionString));
-            
-            services.AddDbContext<MisMstrContext>(options =>
-                options.UseSqlServer(connectionString));
-
-            AddInfrastructureHealthChecks(services);
-
-            // Authentication
-            services.AddCustomAuthorization(config);
 
             return services;
         }
--- Dfe.Academies.Application/Common/Models/MemberOfParliament.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Dfe.Academies.Application.Common.Models
-{
-    public class MemberOfParliament : Person
-    {
-        public required string ConstituencyName { get; set; }
-    }
-}
